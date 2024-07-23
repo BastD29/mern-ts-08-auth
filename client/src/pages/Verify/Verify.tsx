5c222d77a7ff99288f1ad27a19a2b2218441a92a
BastD29 <bastiendaniel9@gmail.com>
2024-07-23 19:34:19 +0200

    added frontend verify token logic & refactored verify service

--- client/src/pages/Verify/Verify.tsx
+++ client/src/pages/Verify/Verify.tsx
@@ -1,10 +1,37 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { verify } from "../../services/verify";
 import style from "./Verify.module.scss";
 
 const Verify: FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    console.log("query:", query);
+
+    const token = query.get("token");
+    console.log("token:", token);
+
+    const verifyToken = async () => {
+      try {
+        const response = await verify(token as string);
+        console.log("response:", response);
+      } catch (error) {
+        console.error("Error verifying token:", error);
+        throw new Error("Verification failed. Please try again");
+      }
+    };
+
+    if (token) {
+      verifyToken();
+    }
+  }, [location]);
+
   return (
     <div className={style["verify"]}>
       <h2>Verify</h2>
+      {/* <p>{message}</p> */}
     </div>
   );
 };
--- client/src/services/verify.ts
+++ client/src/services/verify.ts
@@ -1,26 +1,18 @@
 import { BASE_URL, VERIFY } from "../constants/endpoints";
 
-const verify = async (token: string) => {
+const verify = async (token: string): Promise<string> => {
   console.log("token:", token);
 
   try {
     const url = `${BASE_URL}${VERIFY}?token=${token}`;
     console.log("url:", url);
 
-    const response = await fetch(url, {
-      method: "GET",
-      //   headers: {
-      //     Authorization: `Bearer ${token}`,
-      //   },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(url);
 
     console.log("response:", response);
 
     if (!response.ok) {
-      throw new Error("Failed to verify");
+      throw new Error("Verification failed");
     }
 
     const data = await response.json();
@@ -28,8 +20,8 @@ const verify = async (token: string) => {
 
     return data;
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error("Error verifying token:", error);
+    throw new Error("Verification failed. Please try again");
   }
 };
 
