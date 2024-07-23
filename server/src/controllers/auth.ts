3a8497458b3a936a25f0d48dde28cc3a3ba8ca7d
BastD29 <bastiendaniel9@gmail.com>
2024-07-23 12:39:09 +0200

    added verify controller

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { USERS } from "../data/users";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "../config/environments";
+
+interface AuthTokenPayload extends JwtPayload {
+  userId: string;
+}
 
 const login = async (req: Request, res: Response) => {
   const user = USERS.find((u) => u.email === req.body.email);
@@ -17,12 +21,35 @@ const login = async (req: Request, res: Response) => {
     });
 
     // await sendMagicLinkEmail({ email: user.email, token });
+    res.status(200).json({ message: "Check your email to finish logging in" });
   } catch (error) {
     return res.send("Error logging in. Please try again");
   }
-
-  // res.send("Check your email to finish logging in");
-  res.status(200).json({ message: "Check your email to finish logging in" });
 };
 
-export { login };
+const verify = async (req: Request, res: Response) => {
+  const token = req.query.token as string;
+  if (token == null) return res.sendStatus(401);
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    if (!JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined");
+    }
+
+    const decodedToken = jwt.verify(
+      token,
+      JWT_SECRET
+    ) as unknown as AuthTokenPayload;
+    const userId = Number(decodedToken.userId);
+    const user = USERS.find((u) => u.id === userId);
+    res.send(`Authed as ${user?.name}`);
+  } catch (error) {
+    res.sendStatus(401);
+  }
+};
+
+export { login, verify };
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/auth";
+import { login, verify } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/verify", verify);
 
 export default router;
