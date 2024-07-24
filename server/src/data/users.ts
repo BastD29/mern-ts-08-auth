62ee36c919ab44e380a6167098cd68607f0de6ff
BastD29 <bastiendaniel9@gmail.com>
2024-07-24 14:47:39 +0200

    commented out users.ts

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -1,5 +1,6 @@
 import { UserType } from "../types/user";
 
+// * simulates a database
 const USERS: UserType[] = [
   // { id: 1, email: "moyef23642@vasomly.com", name: "test" },
   { id: 1, email: "jepof52517@tiervio.com", name: "test" },
