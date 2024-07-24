49b22df3e3c30269003399929f1ee086826bee9e
BastD29 <bastiendaniel9@gmail.com>
2024-07-24 16:43:31 +0200

    updated users

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -2,7 +2,6 @@ import { UserType } from "../types/user";
 
 // * simulates a database
 const USERS: UserType[] = [
-  // { id: 1, email: "moyef23642@vasomly.com", name: "test" },
   { id: 1, email: "jepof52517@tiervio.com", name: "test" },
 ];
 
